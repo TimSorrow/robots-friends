2bb099cf2507f730490cbfce5a14a9df7a70cfb6
Tim Sorrow <timursurov@gmail.com>
2022-04-10 16:02:11 +0600

    Updates

---
+++ precache-manifest.28ae9416c442146da1ffc923006a82c9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b4a27b0fbe135b39552",
+    "url": "/robots-friends/static/css/main.c04d7a5e.chunk.css"
+  },
+  {
+    "revision": "1b4a27b0fbe135b39552",
+    "url": "/robots-friends/static/js/main.1b4a27b0.chunk.js"
+  },
+  {
+    "revision": "206e060bd7d3afd2158d",
+    "url": "/robots-friends/static/js/1.206e060b.chunk.js"
+  },
+  {
+    "revision": "bfbbc63234de230c6b8d",
+    "url": "/robots-friends/static/css/2.dafd06c2.chunk.css"
+  },
+  {
+    "revision": "bfbbc63234de230c6b8d",
+    "url": "/robots-friends/static/js/2.bfbbc632.chunk.js"
+  },
+  {
+    "revision": "0e6b42b78ca8bac0084a",
+    "url": "/robots-friends/static/js/runtime~main.0e6b42b7.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robots-friends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "f408bcdb7130c74681cbdedc591c685d",
+    "url": "/robots-friends/index.html"
+  }
+];
