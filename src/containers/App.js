db0b262ffea8d5581f72745575d642336c864546
Timur Surov <52236889+TimSorrow@users.noreply.github.com>
2022-05-22 22:03:09 +0100

    Change App function

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,31 +1,28 @@
-import React, {Component} from "react";
+import React, { useState, useEffect } from "react";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox.js";
 import Scroll from '../components/Scroll';
 import ErrorBoundary from "../components/ErrorBoundry";
 import './App.css';
 
+function App() {
+//    constructor() {
+//        super()
+//        this.state = {
+//            robots: [],
+//            searchfield: ''
+//        }
+//    }
 
-class App extends Component {
-    constructor() {
-        super()
-        this.state = {
-            robots: [],
-            searchfield: ''
-        }
-    }
+//    componentDidMount() {
+//        fetch('https://jsonplaceholder.cypress.io/users')
+//        .then(response => response.json())
+//        .then(users => this.setState({ robots: users}));
+//    }
 
-    componentDidMount() {
-        fetch('https://jsonplaceholder.cypress.io/users')
-        .then(response => response.json())
-        .then(users => this.setState({ robots: users}));
-    }
-
-    onSearchChange = (event) => {
+    const onSearchChange = (event) => {
         this.setState({ searchfield: event.target.value })
     }
-
-    render() {
         const {robots, searchfield} = this.state;
         const filteredRobots = robots.filter(robot => {
             return robot.name.toLowerCase().includes(searchfield.toLowerCase());
@@ -35,16 +32,15 @@ class App extends Component {
         (
             <div className='tc'>
             <h1 className='f1'>RoboFriends</h1> 
-            <SearchBox searchChange={this.onSearchChange}/>
+            <SearchBox searchChange={onSearchChange}/>
             <Scroll>
                 <ErrorBoundary>
                     <CardList robots={filteredRobots}/>
                 </ErrorBoundary>
             </Scroll>
             </div>
-            );
-        }
-}
+         );
+    }
 
 
 export default App;
