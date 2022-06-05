cc26580229d1e71ba484abfa962ffc0521663fda
Tim Sorrow <timursurov@gmail.com>
2022-06-05 12:52:29 +0100

    Fix context errors

--- src/actions.js
+++ src/actions.js
@@ -1,6 +1,6 @@
-import { CHANGE_SEARCHFIELD } from "./constants"
+import { CHANGE_SEARCH_FIELD } from "./constants"
 
 export const setSearchfield = (text) => ({
-    type: CHANGE_SEARCHFIELD, 
+    type: CHANGE_SEARCH_FIELD, 
     payload: text
 })
--- src/reducers.js
+++ src/reducers.js
@@ -1,12 +1,14 @@
-import { CHANGE_SEARCHFIELD } from "./constants"
+import { CHANGE_SEARCH_FIELD } from "./constants.js"
 
 const initialState = { 
     searchField: ''
 }
 
 export const searchRobots = (state = initialState, action = {}) => {
-    switch(action) {
-        case: CHANGE_SEARCHFIELD:
-        return Object.assign({}, state, {searchField: action.payload})
+    switch(action.type) {
+        case CHANGE_SEARCH_FIELD:
+            return Object.assign({}, state, {searchField: action.payload});
+        default:
+            return state;
     }
 }
